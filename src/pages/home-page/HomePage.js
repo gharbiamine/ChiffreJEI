52e8c04fc036f1fb896f405fbf46ec99e6b0ffb6
gharbiamine <aminegharbi@insat.u-carthage.tn>
2021-11-22 11:36:40 +0100

    added homepage

---
+++ src/pages/home-page/HomePage.js
@@ -0,0 +1,77 @@
+import React from "react";
+import styles from "./homePage.module.css";
+import { FullPage, Slide } from "react-full-page";
+import PanelRight from "../../Components/panels/foreground-panels/PanelRight";
+import PanelLeft from "../../Components/panels/foreground-panels/PanelLeft";
+import BackgroundPanel from "../../Components/panels/background-panels/BackgroundPanel";
+
+export const HomePage = () => {
+  return (
+    <>
+      <div className={styles.App}>
+        <FullPage>
+          <Slide>
+            <div className={styles.fullscreen}>
+              <BackgroundPanel side={"left"} />
+              <BackgroundPanel side={"right"} />
+              <PanelLeft
+                title={"Projets réalisés"}
+                stat={{ start: 1, end: 20, delay: 0, duration: 2 }}
+              />
+              <PanelRight
+                title={"Pourcentage el rojla"}
+                stat={{
+                  start: 1,
+                  end: 100,
+                  delay: 0,
+                  duration: 2,
+                  suffix: "%",
+                }}
+              />
+            </div>
+          </Slide>
+          <Slide>
+            <div className={styles.fullscreen}>
+              <BackgroundPanel side={"left"} />
+              <BackgroundPanel side={"right"} />
+              <PanelLeft
+                title={"Projets réalisés"}
+                stat={{ start: 1, end: 20, delay: 0, duration: 2 }}
+              />
+              <PanelRight
+                title={"Pourcentage el rojla"}
+                stat={{
+                  start: 1,
+                  end: 100,
+                  delay: 0,
+                  duration: 2,
+                  suffix: "%",
+                }}
+              />
+            </div>
+          </Slide>
+          <Slide>
+            <div className={styles.fullscreen}>
+              <BackgroundPanel side={"left"} />
+              <BackgroundPanel side={"right"} />
+              <PanelLeft
+                title={"Projets réalisés"}
+                stat={{ start: 1, end: 20, delay: 0, duration: 2 }}
+              />
+              <PanelRight
+                title={"Pourcentage el rojla"}
+                stat={{
+                  start: 1,
+                  end: 100,
+                  delay: 0,
+                  duration: 2,
+                  suffix: "%",
+                }}
+              />
+            </div>
+          </Slide>
+        </FullPage>
+      </div>
+    </>
+  );
+};
